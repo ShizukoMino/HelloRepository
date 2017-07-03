9e77e2cbeeb7584e3b1b0178ee7c96622e489323
mino <mino@AMBC-PC13>
2017-07-03 18:20:05 +0900

    テスト　三野

---
+++ src/main/java/com/example/SampleSpringbootApplication/HelloController.java
@@ -0,0 +1,47 @@
+package com.example.SampleSpringbootApplication;
+
+//import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	//ModelAndViewを使用する
+	@RequestMapping(value = "/")
+	public ModelAndView indexMav(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "Hello World");
+		return  mav;
+	}
+	
+	//Modelを使用する
+	@RequestMapping(value = "/model")
+	public String indexModel(Model model) {
+		model.addAttribute("msg2", "Hello World (Model)");
+	//	model.addAttribute("now", new Date().toString());	//現在時刻の表示
+		return "index";
+	}
+	
+	//URLに入力した値をModelAndViewに渡して画面に表示する
+	@RequestMapping(value = "/{str}")
+	public ModelAndView indexStr(ModelAndView mav, @PathVariable String str) {
+		mav.setViewName("index");
+		mav.addObject("msg", "Hello World");
+		mav.addObject("moji", "入力された文字は「　" + str  + "　」です。");
+		return  mav;
+	}
+	
+	//URLに入力した値をModelに渡して画面に表示する
+	@RequestMapping(value = "/model/{str}")
+	public String indexStrModel(Model model, @PathVariable String str) {
+		model.addAttribute("msg2", "Hello World (Model)");
+		model.addAttribute("moji2", "入力された文字は「　" + str  + "　」です。");
+		return  "index";
+	}
+	
+}
