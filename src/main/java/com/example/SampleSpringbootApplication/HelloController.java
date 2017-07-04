b72b781aa8c5915191b646fa206ae2c4efe2f321
ShizukoMino <mino_s@aztecs-sys.com>
2017-07-04 17:34:05 +0900

    first commit

--- src/main/java/com/example/SampleSpringbootApplication/HelloController.java
+++ src/main/java/com/example/SampleSpringbootApplication/HelloController.java
@@ -1,47 +1,33 @@
 package com.example.SampleSpringbootApplication;
 
-//import java.util.Date;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	//ModelAndViewを使用する
-	@RequestMapping(value = "/")
-	public ModelAndView indexMav(ModelAndView mav) {
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public ModelAndView form(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg", "Hello World");
+		mav.addObject("msg", "Hello, please type...");
 		return  mav;
 	}
 	
-	//Modelを使用する
-	@RequestMapping(value = "/model")
-	public String indexModel(Model model) {
-		model.addAttribute("msg2", "Hello World (Model)");
-	//	model.addAttribute("now", new Date().toString());	//現在時刻の表示
-		return "index";
-	}
-	
-	//URLに入力した値をModelAndViewに渡して画面に表示する
-	@RequestMapping(value = "/{str}")
-	public ModelAndView indexStr(ModelAndView mav, @PathVariable String str) {
-		mav.setViewName("index");
-		mav.addObject("msg", "Hello World");
-		mav.addObject("moji", "入力された文字は「　" + str  + "　」です。");
-		return  mav;
-	}
-	
-	//URLに入力した値をModelに渡して画面に表示する
-	@RequestMapping(value = "/model/{str}")
-	public String indexStrModel(Model model, @PathVariable String str) {
-		model.addAttribute("msg2", "Hello World (Model)");
-		model.addAttribute("moji2", "入力された文字は「　" + str  + "　」です。");
-		return  "index";
+	@RequestMapping(value="/result", method = RequestMethod.POST)
+	public ModelAndView send(ModelAndView mav, @RequestParam String text) {
+		mav.setViewName("index2");
+		
+		//テンプレート側で大文字にする
+		mav.addObject("result", text);		
+		
+		//追加　Javaで大文字にする処理
+		String upText = text.toUpperCase();
+		mav.addObject("result2", upText);
+
+		return mav;
 	}
 	
 }
